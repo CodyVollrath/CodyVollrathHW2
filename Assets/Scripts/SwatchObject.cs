b26caebfb826fd1b5d7a58c0c25ba56a967eaef6
atlasnodes7 <cvollra1@my.westga.edu>
2022-01-21 01:56:09 -0500

    Finished implementation of the logic for color swaps

--- Assets/Scripts/SwatchObject.cs
+++ Assets/Scripts/SwatchObject.cs
@@ -5,29 +5,31 @@ using UnityEngine;
 public class SwatchObject : MonoBehaviour
 {
     public GameObject colorObjects;
-    private bool toggle = true;
+    public GameObject otherSwatch1;
+    public GameObject otherSwatch2;
     public void OnMouseDown()
     {
         float scaler = 0.4f;
         var renderer = GetComponent<Renderer>();
-        
-        if (toggle)
+        var thisColor = renderer.material.color;
+        var mainColorObj = colorObjects.GetComponent<Renderer>();
+        if (!thisColor.Equals(mainColorObj.material.color))
         {
             transform.localScale = new Vector3(transform.localScale.x + scaler, transform.localScale.y + scaler, transform.localScale.z + scaler);
-            var mainColorObj = colorObjects.GetComponent<Renderer>();
             mainColorObj.material.color = renderer.material.color;
-            foreach (Transform gameObj in colorObjects.transform) 
+            foreach (Transform gameObj in colorObjects.transform)
             {
                 var colorObj = gameObj.GetComponent<Renderer>();
                 colorObj.material.color = renderer.material.color;
             }
-            toggle = false;
         }
-        else 
+        if (otherSwatch1.transform.localScale.x > 1)
         {
-            transform.localScale = new Vector3(transform.localScale.x - scaler, transform.localScale.y - scaler, transform.localScale.z - scaler);
-            
-            toggle = true;
+            otherSwatch1.transform.localScale = new Vector3(transform.localScale.x - scaler, transform.localScale.y - scaler, transform.localScale.z - scaler);
+        }
+        if (otherSwatch2.transform.localScale.x > 1)
+        {
+            otherSwatch2.transform.localScale = new Vector3(transform.localScale.x - scaler, transform.localScale.y - scaler, transform.localScale.z - scaler);
         }
 
     }
